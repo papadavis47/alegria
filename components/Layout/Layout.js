0d7296069a787201f980c56a8a04b9c5483eaab3
John William Davis <jwdavisdeveloper@gmail.com>
2022-05-19 09:54:20 -0700

    Scaffold out the Navbar, add logo

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,13 +1,15 @@
 import Head from 'next/head'
 import Footer from '../Footer'
+import Navbar from '../Navbar'
 
 function Layout({ children }) {
   return (
-    <div className="flex flex-col items-center min-h-screen py-2">
+    <div className="flex flex-col min-h-screen py-2">
       <Head>
         <title>Alegria</title>
         <link rel="icon" href="/surfing.ico" />
       </Head>
+      <Navbar />
       <main className="flex flex-col items-center flex-1 w-full px-20 text-center">
         {children}
       </main>
--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,7 +1,36 @@
-import React from "react";
+import Link from 'next/link'
+import Image from 'next/image'
 
 function Navbar() {
-  return <div />;
+  return (
+    <nav className="bg-white shadow-lg">
+      <div className="px-4 mx-auto max-w-7xl">
+        <div className="flex justify-between">
+          <div className="flex space-x-7">
+            <Link href="/">
+              <a className="flex items-center px-2 py-4">
+                <Image src="/images/logo.png" width={60} height={60} />
+                <span className="ml-4 text-lg font-bold text-orange-400 uppercase">
+                  alegria
+                </span>
+              </a>
+            </Link>
+          </div>
+          <div>
+            <Link href="#">
+              <a>About</a>
+            </Link>
+            <Link href="#">
+              <a>Gratitude List</a>
+            </Link>
+            <Link href="#">
+              <a>Inspirations</a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
-export default Navbar;
+export default Navbar
