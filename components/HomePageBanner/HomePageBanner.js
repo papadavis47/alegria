b68620f38797c454a2558ed720efbc0f8fda21cb
John William Davis <jwdavisdeveloper@gmail.com>
2022-05-20 16:59:06 -0700

    Change some copy on HomePageBanner

--- components/HomePageBanner/HomePageBanner.js
+++ components/HomePageBanner/HomePageBanner.js
@@ -4,10 +4,11 @@ function HomePageBanner() {
       <div className="sm:text-center lg:text-left">
         <h1 className="text-4xl font-extrabold tracking-tight capitalize sm:text-5xl md:text-6xl">
           <span className="block text-orange-300">light your path</span>
-          <span className="block text-orange-500">
-            remember the important things
-          </span>
+          <span className="block text-orange-500">keep moving forward</span>
         </h1>
+        <h2 className="my-3 text-2xl font-extrabold tracking-tight text-orange-400 capitalize sm:text-3xl md:text-4xl">
+          remind yourself of the things that really matter
+        </h2>
         <p className="my-3 text-base text-orange-400 text-semibold sm:mx-auto sm:max-w-xl sm:text-lg md:max-w-4xl md:text-xl lg:mx-0 lg:max-w-6xl">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Neque labore
           similique rem, dolorem cumque sequi. Nam provident adipisci amet
--- notes.md
+++ notes.md
@@ -5,3 +5,7 @@
 <span class="block">What do I dream about?</span>
 <span class="block">Where do I want to go?</span>
 ```
+
+Maybe use the phrase: "Keep moving forward" - in place of "Remember the important things"
+
+Just an idea. We'll see.
