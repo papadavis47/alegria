0f0c1deb7e7a4be2ada8430c52d1beb3ca327002
John William Davis <jwdavisdeveloper@gmail.com>
2022-05-21 02:32:26 -0700

    Work on new QuestionsContainer component - begin styling

--- components/QuestionsContainer/QuestionsContainer.js
+++ components/QuestionsContainer/QuestionsContainer.js
@@ -1,7 +1,39 @@
-import React from 'react'
-
 function QuestionsContainer() {
-  return <div>This will be my questions container</div>
+  return (
+    <div className="px-4 pb-8 mx-auto my-10 max-w-7xl lg:px-8">
+      <div>
+        <h1 className="mb-3 text-4xl font-extrabold tracking-tight text-sky-600 sm:text-center sm:text-5xl md:text-6xl lg:text-left">
+          Make Notes
+        </h1>
+        <p className="mb-3 text-base font-semibold text-sky-500 sm:mx-auto sm:max-w-xl sm:text-lg md:max-w-4xl md:text-xl lg:mx-0 lg:max-w-6xl">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+          necessitatibus eum veniam dolorem aperiam cum voluptates nulla
+          reprehenderit dolore.
+        </p>
+      </div>
+      <div>
+        <div>
+          <h2>What makes me happy</h2>
+        </div>
+        <div>
+          <h2>What inspires me?</h2>
+        </div>
+        <div>
+          <h2>What am I grateful for?</h2>
+        </div>
+        <div>
+          <h2>What do I dream about?</h2>
+        </div>
+        <div>
+          <h2>Where do I want to go?</h2>
+        </div>
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum saepe
+        distinctio iusto? Aut omnis totam hic iure.
+      </p>
+    </div>
+  )
 }
 
 export default QuestionsContainer
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import HomePageBanner from '../components/HomePageBanner'
+import QuestionsContainer from '../components/QuestionsContainer'
+
 const Home = () => {
   return (
     <>
       <HomePageBanner />
+      <QuestionsContainer />
     </>
   )
 }
